48058eedad9027bbe6b4c829abb6c19983850330
Alex-eevich <alex.2323655@mail.ru>
2022-12-20 12:57:47 +0300

    Ubuntu Docker

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-import socket
 
-host = socket.gethostname()
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"host": host}
+    return {"Hello": "World"}
